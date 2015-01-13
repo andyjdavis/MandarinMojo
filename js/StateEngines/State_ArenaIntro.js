d3f1fff14b0d03f3deb006aaf119c7ea4346a62e
Andrew Davis <andrew@moodle.com>
2015-01-13 19:02:49 +0100

    added message if enter review when it is empty

--- js/StateEngines/State_ArenaIntro.js
+++ js/StateEngines/State_ArenaIntro.js
@@ -23,25 +23,41 @@ game.State_ArenaIntro.prototype.draw = function() {
     if (img) {
         gContext.drawImage(img, 0, 0);
     }
-
     x = gCanvas.width/2;
-    drawText(gContext, "Collect the correct characters", gWorld.textsize, gWorld.textcolor, x, 150);
-    drawText(gContext, "Avoid the critters", gWorld.textsize, gWorld.textcolor, x, 180);
-    //drawText(gContext, "Use the arrow keys to move", gWorld.textsize, gWorld.textcolor, x, 210);
 
-    //drawText(gContext, "Press m to mute sound effects", gWorld.textsize, gWorld.textcolor, x, 240);
-    drawText(gContext, "Press p to pause", gWorld.textsize, gWorld.textcolor, x, 310);
-    drawText(gContext, "Press e to begin", gWorld.textsize, "white", x, 340);
+    // Can't use this.wordcount as it is set on the arena by setLevel().
+    if (this.level == 0 && gWorld.playerinfo.problems.length == 0) {
+        drawText(gContext, "Your review queue is empty", gWorld.textsize, gWorld.textcolor, x, 150);
+        drawText(gContext, "Attempt a HSK level and words will be added", gWorld.textsize, gWorld.textcolor, x, 180);
+        drawText(gContext, "Press e to exit the review arena", gWorld.textsize, "white", x, 340);
+    } else {
+        drawText(gContext, "Collect the correct characters", gWorld.textsize, gWorld.textcolor, x, 150);
+
+        var s = "Avoid the critters";
+        if (this.level == 0) {
+            s = "Review in safety. No monsters here.";
+        }
+        drawText(gContext, s, gWorld.textsize, gWorld.textcolor, x, 180);
+
+        //drawText(gContext, "Press m to mute sound effects", gWorld.textsize, gWorld.textcolor, x, 240);
+        drawText(gContext, "Press p to pause", gWorld.textsize, gWorld.textcolor, x, 310);
+        drawText(gContext, "Press e to begin", gWorld.textsize, "white", x, 340);
+    }
 };
 game.State_ArenaIntro.prototype.update = function(dt) {
 };
 game.State_ArenaIntro.prototype.onKeyDown = function(event) {
     // "e"
     if (event.keyCode == 69) {
-        var state = gWorld.state.setState(gWorld.state.states.ARENA);
-        state.wordindex = this.wordindex;
-        state.wordcount = this.wordcount;
-        state.setLevel(this.level);
+        // Can't use this.wordcount as it is set on the arena by setLevel().
+        if (this.level == 0 && gWorld.playerinfo.problems.length == 0) {
+            var state = gWorld.state.setState(gWorld.state.states.MAP);
+        } else {
+            var state = gWorld.state.setState(gWorld.state.states.ARENA);
+            state.wordindex = this.wordindex;
+            state.wordcount = this.wordcount;
+            state.setLevel(this.level);
+        }
     }
 };
 
